a227f76458c5fcf1ef74cd19f1e2abbb3f8b3cfa
vladislavs-v@yandex.ru <29291720152312aa>
2019-06-30 13:51:01 +0300

    change to Todos

--- src/Todos.java
+++ src/Todos.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 
+@SuppressWarnings("ALL")
 public class Todos {
     public static void main(String[] args) {
-        System.out.println("Введите задачу");
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println("1." + s);
+        String b = "Введите задачу";
+        int n = 1;
+        while (true) {
+            System.out.println(b);
+            Scanner sc = new Scanner(System.in);
+
+            String s = sc.nextLine();
+
+            System.out.println(n + "." + s);
+            n++;
+        }
     }
 }
