27a611abd5aeba641acf924241108181d71d97e1
vladislavs-v@yandex.ru <29291720152312aa>
2019-06-30 17:53:51 +0300

    add cycle exit

--- src/Todos.java
+++ src/Todos.java
@@ -9,10 +9,32 @@ public class Todos {
             System.out.println(b);
             Scanner sc = new Scanner(System.in);
 
-            String s = sc.nextLine();
+            String s = sc.next();
 
             System.out.println(n + "." + s);
             n++;
+            //String s1 = sc.next();
+            if (s.equals("exit")) {
+                break;
+            }
+
+
+        }
+
+    }
+
+    public class Color {
+        public void Search() {
+
+            String a = "Выберете задачу";
+            int n = 1;
+            while (true) {
+                System.out.println(a);
+                Scanner s = new Scanner(System.in);
+                int number = s.nextInt();
+                System.out.println("Вы выбрали задачу " + s);
+            }
+
         }
     }
 }
